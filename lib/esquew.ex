b867f4d859ce5d4de486fcbaae12dadee9b19cda
bwireman <bwireman1@gmail.com>
2020-09-19 13:50:32 -0400

    clean up project

--- lib/esquew.ex
+++ lib/esquew.ex
@@ -1,18 +1,2 @@
 defmodule Esquew do
-  @moduledoc """
-  Documentation for `Esquew`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Esquew.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
